00f1e128659eadb6b2be6f8f0eed4ea8b4238dbf
katepopkova <ekaterina.grishnova@mail.ru>
2020-07-01 02:37:57 +0300

    RandomNumberGenerator has been added

--- src/main/java/com/randnumber/controller/RandomNumberController.java
+++ src/main/java/com/randnumber/controller/RandomNumberController.java
@@ -1,23 +1,22 @@
 package com.randnumber.controller;
 
+import com.randnumber.service.RandomNumberGenerator;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
 import java.util.*;
 
 @RestController
 @RequestMapping("randnumber")
 public class RandomNumberController {
-    private static final int FIRST_ID = 1;
-    private static final int HIGHEST_RANDOM_BOUND = 101;
 
-    //a number is in JSON format with it's ID
-    private List<Map<String, Integer>> number = new ArrayList<Map<String, Integer>>() {{
-        add(new LinkedHashMap<String, Integer>() {{
-            put("id", FIRST_ID);
-            put("number", new Random().nextInt(HIGHEST_RANDOM_BOUND));}});
-    }};
+    @Autowired
+    private RandomNumberGenerator randomNumberGenerator;
 
     @GetMapping
     public List<Map<String, Integer>> getNumber() {
-        return number;
+        int number = randomNumberGenerator.generateRandomNumber();
+        List<Map<String, Integer>> numJsonFormat = randomNumberGenerator.toJsonFormat(number);
+        return numJsonFormat;
     }
 }
---
+++ src/main/java/com/randnumber/service/RandomNumberGenerator.java
@@ -0,0 +1,27 @@
+package com.randnumber.service;
+
+import org.springframework.stereotype.Service;
+
+import java.util.*;
+
+@Service
+public class RandomNumberGenerator {
+    private static final int HIGHEST_RANDOM_BOUND = 101;
+    private int id;
+
+    public int generateRandomNumber() {
+        ++id;
+       return new Random().nextInt(HIGHEST_RANDOM_BOUND);
+    }
+
+    public List<Map<String, Integer>> toJsonFormat(int value) {
+        //a number is in JSON format with it's ID
+        List<Map<String, Integer>> number = new ArrayList<Map<String, Integer>>() {{
+            add(new LinkedHashMap<String, Integer>() {{
+                put("id", id);
+                put("number", value);}});
+        }};
+
+        return number;
+    }
+}
